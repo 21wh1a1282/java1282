d1329a63afe9a3e70ffeeeaa0efb5ab879f71698
21wh1a1282 <113002089+21wh1a1282@users.noreply.github.com>
2023-07-14 10:43:31 +0530

    Add files via upload

---
+++ AppletExample.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import java.applet.*;
+
+/* <applet code="Applet1" width=200 height=300></applet>*/
+
+ public class AppletExample extends Applet
+{
+
+        public void paint(Graphics g)
+        {
+                g.drawString("Hello World!",20,20);
+        }
+
+}
+
